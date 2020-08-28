b70e56b807e4c9518bc128539cab7c022e79393e
NGK <nannaklukog12345@gmail.com>
2020-08-29 01:45:47 +0530

    added files

---
+++ SpeakHello.js
@@ -0,0 +1,11 @@
+(function (window) {
+	var helloSpeaker = {};
+	helloSpeaker.speak=function () {
+		
+	}
+	var speakWord="Hello";
+	helloSpeaker.speak = function (name) {
+		console.log(speakWord + " " + name);
+	}
+	window.helloSpeaker=helloSpeaker;
+})(window);
